31ee2078f513440b5e80c0243e3311a7d6149185
Michael <104983553+Michael-Johansson@users.noreply.github.com>
2022-11-09 22:28:53 +0100

    completely remade the whole ui, cause why not

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+
+
 function getComputerChoice() {
   const computerChoices = ["rock", "paper", "scissors"];
   return computerChoices[Math.trunc(Math.random() * computerChoices.length)];
 }
 
-let playerScore = 0;
-let computerScore = 0;
-
 function playRound(playerSelection, computerSelection) {
+
   if (playerSelection == "rock" && computerSelection == "scissors") {
     playerScore++;
     return "You win!";
@@ -34,25 +34,4 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  for (let i = 0; i <= 4; i++) {
-    const playerSelection = prompt("What's your pick?").toLowerCase();
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-    console.log(
-      `You picked ${playerSelection} and Computer picked ${computerSelection}`
-    );
-    console.log(
-      `Your score is ${playerScore} and Computer score is ${computerScore}`
-    );
 
-    console.log(" ");
-    if (i == 4 && playerScore > computerScore) {
-      console.log("Congratulations, you won the game");
-    } else if (i == 4 && computerScore > playerScore) {
-      console.log("Unfortunately, you lost the game");
-    } else if (i == 4 && playerScore == computerScore) {
-      console.log("This game resulted in a tie!");
-    }
-  }
-}
