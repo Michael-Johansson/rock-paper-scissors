213a2a8685c845124b48b5e465459dacd00941be
Michael <104983553+Michael-Johansson@users.noreply.github.com>
2022-11-12 02:41:34 +0100

    small tweaks all over the place

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll(".button");
 const heading = document.querySelector(".heading");
-const player = document.querySelector(".player-score");
-const computer = document.querySelector(".computer-score");
+const playerScoreEl = document.querySelector(".player-score");
+const computerScoreEl = document.querySelector(".computer-score");
 const playerChoice = document.querySelector("#player-choice");
 const computerChoice = document.querySelector("#computer-choice");
 const wrapper = document.querySelector(".wrapper");
-const selections = document.querySelector(".selections");
-const playerButtons = document.querySelector(".player-buttons");
 let playerScore = 0;
 let computerScore = 0;
 let result = "";
 
+// Animating the wrapper when document has loaded
 window.onload = () => {
   wrapper.classList.add("wrapper-fade-in");
   wrapper.classList.remove("wrapper-hidden");
@@ -20,100 +19,144 @@ const playerWin = () => {
   playerScore++;
   result = "You win!";
 };
+
 const computerWin = () => {
   computerScore++;
   result = "You lost!";
 };
 
-function game() {
-  function getComputerChoice() {
-    const computerChoices = ["rock", "paper", "scissors"];
-    return computerChoices[Math.trunc(Math.random() * computerChoices.length)];
-  }
+const playAnimation = () => {
+  computerChoice.classList.toggle("computer-active");
+  playerChoice.classList.toggle("player-active");
+};
 
-  function playRound(playerSelection, computerSelection) {
-
-    if (playerSelection == "rock" && computerSelection == "scissors") {
-      playerWin();
-      playerChoice.src = `images/${playerSelection}.png`;
-      computerChoice.src = `images/${computerSelection}.png`;
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
-      computerWin();
-      playerChoice.src = `images/${playerSelection}.png`;
-      computerChoice.src = `images/${computerSelection}.png`;
-    }
-    if (playerSelection == "paper" && computerSelection == "rock") {
-      playerWin();
-      playerChoice.src = `images/${playerSelection}.png`;
-      computerChoice.src = `images/${computerSelection}.png`;
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
-      computerWin();
-      playerChoice.src = `images/${playerSelection}.png`;
-      computerChoice.src = `images/${computerSelection}.png`;
-    }
-    if (playerSelection == "scissors" && computerSelection == "paper") {
-      playerWin();
-      playerChoice.src = `images/${playerSelection}.png`;
-      computerChoice.src = `images/${computerSelection}.png`;
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-      computerWin();
-      playerChoice.src = `images/${playerSelection}.png`;
-      computerChoice.src = `images/${computerSelection}.png`;
-    }
-
-    if (playerSelection == computerSelection) {
-      playerChoice.src = `images/${playerSelection}.png`;
-      computerChoice.src = `images/${computerSelection}.png`;
-      playerScore;
-      computerScore;
-      result = "Tie!";
-    }
-  }
-
+const disableButtons = () => {
   buttons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      const computerSelection = getComputerChoice();
-      waitForComputer();
-      computerChoice.classList.toggle("active");
-      playerChoice.classList.toggle("player-active");
-      setTimeout(() => {
-        playRound(e.target.id, computerSelection);
-      }, 1000);
-      buttons.forEach((button) => {
-        button.disabled = true;
-      });
-    });
+    button.disabled = true;
   });
+};
 
-  function waitForComputer() {
-    setTimeout(() => {
-      heading.textContent = "3";
-    }, 0);
-    setTimeout(() => {
-      heading.textContent = "2";
-    }, 1000);
-    setTimeout(() => {
-      heading.textContent = "1";
-    }, 2000);
-    setTimeout(() => {
+const enableButtons = () => {
+  buttons.forEach((button) => {
+    button.disabled = false;
+  });
+};
 
-      if (playerScore == 5 && computerScore < playerScore) {
-        heading.textContent = "You won the game!"
-      } else if (computerScore == 5 && playerScore < computerScore) {
-        heading.textContent = "You lost the game!"
-      } else {
-        heading.textContent = result;
-      }
-      player.textContent = playerScore;
-      computer.textContent = computerScore;
-      
-      computerChoice.classList.toggle("active");
-      playerChoice.classList.toggle("player-active");
-      buttons.forEach((button) => {
-        button.disabled = false;
-      });
-    }, 3000);
+const restartGame = () => {
+  heading.textContent = "Resetting in 3";
+  setTimeout(() => {
+    heading.textContent = "Resetting in 2";
+  }, 1000);
+
+  setTimeout(() => {
+    heading.textContent = "Resetting in 1";
+  }, 2000);
+
+  setTimeout(() => {
+    heading.textContent = "Rock Paper Scissors";
+    heading.style.color = "var(--main-gray)";
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreEl.textContent = playerScore;
+    computerScoreEl.textContent = computerScore;
+    enableButtons();
+  }, 3000);
+};
+
+function getComputerChoice() {
+  const computerChoices = ["rock", "paper", "scissors"];
+  return computerChoices[Math.trunc(Math.random() * computerChoices.length)];
+}
+
+// Counts down then shows the round results, also checking if anyone won the game
+function waitForComputer() {
+  setTimeout(() => {
+    heading.textContent = "3";
+  }, 0);
+
+  setTimeout(() => {
+    heading.textContent = "2";
+  }, 800);
+
+  setTimeout(() => {
+    heading.textContent = "1";
+  }, 1600);
+
+  setTimeout(() => {
+    if (playerScore == 3) {
+      heading.textContent = "You won the game!";
+      heading.style.color = "#449f5b";
+      disableButtons();
+
+      setTimeout(() => {
+        restartGame();
+      }, 1500);
+    } else if (computerScore == 3) {
+      heading.textContent = "You lost the game!";
+      heading.style.color = "#e86363";
+      disableButtons();
+
+      setTimeout(() => {
+        restartGame();
+      }, 1500);
+    } else {
+      heading.textContent = result;
+      enableButtons();
+    }
+    playerScoreEl.textContent = playerScore;
+    computerScoreEl.textContent = computerScore;
+    playAnimation();
+  }, 2400);
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == "rock" && computerSelection == "scissors") {
+    playerWin();
+    playerChoice.src = `images/${playerSelection}.png`;
+    computerChoice.src = `images/${computerSelection}.png`;
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    computerWin();
+    playerChoice.src = `images/${playerSelection}.png`;
+    computerChoice.src = `images/${computerSelection}.png`;
+  }
+  if (playerSelection == "paper" && computerSelection == "rock") {
+    playerWin();
+    playerChoice.src = `images/${playerSelection}.png`;
+    computerChoice.src = `images/${computerSelection}.png`;
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    computerWin();
+    playerChoice.src = `images/${playerSelection}.png`;
+    computerChoice.src = `images/${computerSelection}.png`;
+  }
+  if (playerSelection == "scissors" && computerSelection == "paper") {
+    playerWin();
+    playerChoice.src = `images/${playerSelection}.png`;
+    computerChoice.src = `images/${computerSelection}.png`;
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    computerWin();
+    playerChoice.src = `images/${playerSelection}.png`;
+    computerChoice.src = `images/${computerSelection}.png`;
+  }
+
+  if (playerSelection == computerSelection) {
+    playerChoice.src = `images/${playerSelection}.png`;
+    computerChoice.src = `images/${computerSelection}.png`;
+    playerScore;
+    computerScore;
+    result = "Tie!";
   }
 }
 
-game();
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const computerSelection = getComputerChoice();
+    waitForComputer();
+
+    disableButtons();
+
+    setTimeout(() => {
+      playRound(e.target.id, computerSelection);
+    }, 1000);
+    playAnimation();
+  });
+});
