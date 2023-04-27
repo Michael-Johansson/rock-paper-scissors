73b9a643aac015be3ae33994d177b06b8d7cb401
Michael <michael_405@outlook.com>
2023-04-27 11:25:18 +0200

    add constants

---
+++ game.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const heading = document.querySelector(".heading");
+const playerSelection = document.querySelector(".player-selection");
+const playerScore = document.querySelector(".player-score");
+const computerSelection = document.querySelector(".computer-selection");
+const computerScore = document.querySelector(".computer-score");
+const currentRound = document.querySelector(".current-round");
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
